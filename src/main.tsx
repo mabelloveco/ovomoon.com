5d79c6ab1f91b904dd64cf0fb54a5a7d62ec4170
mabelloveco <188736915+mabelloveco@users.noreply.github.com>
2025-09-06 03:01:31 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+import App from "./App";            // ← original homepage
 import QA from "./pages/qa";
-import Blog from "./pages/blog";
-
-function Home() {
-  return (
-    <main className="p-8 max-w-3xl mx-auto">
-      <h1 className="text-3xl font-bold mb-6">Ovomoon</h1>
-      <nav className="space-x-4">
-        <Link to="/qa">Q&A</Link>
-        <Link to="/blog">Blog</Link>
-      </nav>
-      <p className="mt-6">Home content.</p>
-    </main>
-  );
-}
+import BlogIndex from "./pages/blog";
+import PostPage from "./pages/post";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<App />} />          {/* home = original App */}
         <Route path="/qa" element={<QA />} />
-        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog" element={<BlogIndex />} />
+        <Route path="/blog/:slug" element={<PostPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
