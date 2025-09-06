ea28e586c922b2ede86b592d3628f512f451f9f8
mabelloveco <188736915+mabelloveco@users.noreply.github.com>
2025-09-06 03:03:17 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App";            // ← original homepage
+import App from "./App";
+import Layout from "./layouts/Layout";   // ← make sure you created Layout.tsx
 import QA from "./pages/qa";
 import BlogIndex from "./pages/blog";
 import PostPage from "./pages/post";
@@ -11,10 +12,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />          {/* home = original App */}
-        <Route path="/qa" element={<QA />} />
-        <Route path="/blog" element={<BlogIndex />} />
-        <Route path="/blog/:slug" element={<PostPage />} />
+        <Route element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="qa" element={<QA />} />
+          <Route path="blog" element={<BlogIndex />} />
+          <Route path="blog/:slug" element={<PostPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
