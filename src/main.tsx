81459f479479269e1aece3a8d847042855666b88
mabelloveco <188736915+mabelloveco@users.noreply.github.com>
2025-09-06 04:03:16 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App";                 // original homepage
-import Layout from "./layouts/layout";   // match your filename
+import App from "./App";                 // your original homepage
+import Layout from "./layouts/layout";   // matches src/layouts/layout.tsx
 import QA from "./pages/qa";
 import BlogIndex from "./pages/blog";
 import PostPage from "./pages/post";
@@ -12,17 +12,16 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-  {/* homepage = your original App, NOT wrapped */}
-  <Route path="/" element={<App />} />
+        {/* Homepage NOT wrapped, so your purple home returns */}
+        <Route path="/" element={<App />} />
 
-  {/* other pages use Layout */}
-  <Route element={<Layout />}>
-    <Route path="qa" element={<QA />} />
-    <Route path="blog" element={<BlogIndex />} />
-    <Route path="blog/:slug" element={<PostPage />} />
-  </Route>
-</Routes>
-
+        {/* Other pages wrapped in Layout */}
+        <Route element={<Layout />}>
+          <Route path="qa" element={<QA />} />
+          <Route path="blog" element={<BlogIndex />} />
+          <Route path="blog/:slug" element={<PostPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
