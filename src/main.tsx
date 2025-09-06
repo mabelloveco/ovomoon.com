2cda245c72de176f31c00ea044d915790b184b88
mabelloveco <188736915+mabelloveco@users.noreply.github.com>
2025-09-06 03:46:08 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App";               // ← your original homepage
-import Layout from "./layouts/Layout"; // ← new shared layout
+import App from "./App";                 // original homepage
+import Layout from "./layouts/layout";   // match your filename
 import QA from "./pages/qa";
 import BlogIndex from "./pages/blog";
 import PostPage from "./pages/post";
@@ -12,8 +12,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        {/* Do NOT wrap home with Layout */}
+        <Route path="/" element={<App />} />
+
+        {/* Everything else uses Layout */}
         <Route element={<Layout />}>
-          <Route index element={<App />} />          {/* home = App */}
           <Route path="qa" element={<QA />} />
           <Route path="blog" element={<BlogIndex />} />
           <Route path="blog/:slug" element={<PostPage />} />
